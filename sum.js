c41277273440abfaa8f26e469e385106a77ee66d
Wittawin1 <winthemath16@gmail.com>
2024-09-11 22:32:16 +0700

    finished

--- sum.js
+++ sum.js
@@ -1,5 +1,7 @@
 function sumTwoNumbers(a, b) {
-    // code here...
+    if(!a || !b || typeof(a) != "number" || typeof(b) != "number")
+       { return null}
+    return a + b
 }
 
 module.exports = sumTwoNumbers;
